1ccf34f0d25b8b3ceae5cb8edcd31f2c4b32dfc3
brayanaraujo <brayanjesusaraujovega@gmail.com>
2025-10-03 13:52:04 -0500

    Pdf explicativo

--- Agenda/src/main/java/br/Agenda/servicio/IcontactoServicio.java
+++ Agenda/src/main/java/br/Agenda/servicio/IcontactoServicio.java
@@ -1,12 +1,12 @@
-package br.Agenda.servicio;
+    package br.Agenda.servicio;
 
-import br.Agenda.modelo.contacto;
+    import br.Agenda.modelo.contacto;
 
-import java.util.List;
+    import java.util.List;
 
-public interface IcontactoServicio {
-public List<contacto> listarContacto();
-public contacto bsucarContactoPorId(Integer idContacto);
-public void guardarContacto(contacto Contacto );
-public contacto eliminarContacto(contacto Contacto);
-}
+    public interface IcontactoServicio {
+    public List<contacto> listarContacto();
+    public contacto bsucarContactoPorId(Integer idContacto);
+    public void guardarContacto(contacto Contacto );
+    public contacto eliminarContacto(contacto Contacto);
+    }
